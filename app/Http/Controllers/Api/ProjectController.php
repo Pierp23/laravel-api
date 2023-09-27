e028558d98a68ea4343ccd3eeb6dfaeffad03edd
Pierp23 <ryu.no.seishin.21@gmail.com>
2023-09-27 15:15:48 +0200

    ADD Api Controller

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+// Model
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+
+        return response()->json([
+            'success' => true,
+            'code' => 200,
+            'message' => 'OK',
+            'projects' => $projects
+        ]);
+    }
+
+    public function show($id)
+    {
+        $project = Project::where('id', $id)->firstOrFail();
+
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'code' => 200,
+                'message' => 'OK',
+                'projects' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'result' => 'Not found'
+            ]);
+        }
+    }
+}
