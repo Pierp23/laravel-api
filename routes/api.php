c680bb0e47125c42a7e4795ef823769ed3715ca2
Pierp23 <ryu.no.seishin.21@gmail.com>
2023-09-27 15:19:45 +0200

    ADD route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ProjectController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::name('projects.')
+    ->prefix('project')
+    ->group(function () {
+
+        Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+            return $request->user();
+        });
+
+        Route::get('/', [ProjectController::class, 'index'])->name('index');
+
+        Route::get('/{post}', [ProjectController::class, 'show'])->name('show');
+    });
